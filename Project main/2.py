ec2392016e39e0e8d8c9baa45e76a1d631625cb4
eliashuber2401 <elias.huber@campus.lmu.de>
2025-07-05 18:33:40 +0200

    change to pandas dataframe

--- Project main/2.py
+++ Project main/2.py
@@ -13,11 +13,13 @@ from courselib.optimizers import GDOptimizer
 from courselib.models.linear_models import LinearBinaryClassification, RidgeClassifier
 from courselib.utils.normalization import min_max
 from courselib.models.glm import LogisticRegression
+from courselib.models.base import TrainableModel
 
 def fetch_data(ticker='SPY', start='2015-01-01', end='2023-12-31'):
     data = yf.download(ticker, start, end)
-    data.dropna(inplace=True)
-    return data
+    df = pd.DataFrame(data)
+    df.columns = df.columns.droplevel(1)
+    return df
 
 
 
@@ -103,78 +105,49 @@ if __name__ == '__main__':
     df['Target'] = np.where(df['Close'].shift(-1) > df['Close'], 1, 0)
     df = compute_technical_indicators(df, 3, 14)
 
-
     features = ['SMA_14', 'EMA_14', 'RSI_14', 'Bollinger_Upper', 'Bollinger_Lower',
                 'MACD', 'MACD_Signal', 'Daily_Return', 'Rolling_5d_Std',
                 'Lag_1', 'Lag_2', 'Lag_3', 'Momentum_10','Target']
+
     df = df[features]
+    df = df.reset_index()
     df.dropna(inplace=True)
-    burn_in_period = 50
-    df = df.iloc[burn_in_period:].copy()
-    df = min_max(df)
-
-
-
-    X, Y, train_X, train_Y, test_X, test_Y = train_test_split(df.iloc[:, df.columns != 'Date'],
-                                                              training_data_fraction=0.7,
-                                                              shuffle = False,
+    X, Y, X_train, Y_train, X_test, Y_test = train_test_split(df.iloc[:, df.columns != 'Date'],
+                                                              training_data_fraction=0.8,
+                                                              return_numpy=True,
                                                               class_column_name='Target')
 
+    X = min_max(X)
+    X_train = min_max(X_train)
+    X_test = min_max(X_test)
 
-
-
-    X = np.nan_to_num(X)
-    train_X = np.nan_to_num(train_X)
-    test_X = np.nan_to_num(test_X)
-    Y = np.nan_to_num(Y)
-    train_Y = np.nan_to_num(train_Y)
-    test_Y = np.nan_to_num(test_Y)
-
-
-
-
-
-    w = [0]*X.shape[1]
+    w = [0] * X.shape[1]
     b = 0
-    optimizer = GDOptimizer(learning_rate=1e-5)
-
-
-    # Define Poisson loss (to match training objective)
+    optimizer = GDOptimizer(learning_rate=1e-2)
 
     accuracy = lambda y_true, y_pred: binary_accuracy(y_true, y_pred, class_labels=[0, 1])
-    metrics_dict = {
-        'accuracy': accuracy,
-        'MSE': mean_squared_error,
-        'MAE': mean_absolute_error
-    }
+    metrics_dict = {'accuracy': accuracy,
+                    'MSE': mean_squared_error}
 
-    model = LinearSVM(w, b, optimizer,C=10)
-    metrics_history = model.fit(train_X,train_Y,
-                                num_epochs=50000,
-                                batch_size=len(train_X),
-                                compute_metrics=True,
+    model = LogisticRegression(w, b, optimizer)
+    metrics_history = model.fit(X_train, Y_train, num_epochs=20000, batch_size=len(X_train), compute_metrics=True,
                                 metrics_dict=metrics_dict)
 
+fig, ax = plt.subplots()
 
-    fig, ax = plt.subplots()
+ax.plot(range(len(metrics_history['MSE'])), metrics_history['MSE'])
+ax.set_ylabel('Loss value')
 
-    ax.plot(metrics_history['accuracy'], label='Accuracy')
-    ax.set_ylabel('Accuracy')
-    ax.set_xlabel('Epoch')
-    ax.grid(True)
+ax2 = ax.twinx()
+ax2.plot(range(len(metrics_history['accuracy'])), metrics_history['accuracy'], color='orange')
+ax2.set_ylabel('Accuracy')
 
-    ax2 = ax.twinx()
-    ax2.plot(metrics_history['MAE'], color='orange', label='MAE')
-    ax2.set_ylabel('Mean Absolute Error')
+ax.set_xlabel('Epoch')
 
-    plt.title('Learning Curve')
-    lines, labels = ax.get_legend_handles_labels()
-    lines2, labels2 = ax2.get_legend_handles_labels()
-    ax2.legend(lines + lines2, labels + labels2, loc='upper right')
+plt.title('Learning curve')
+plt.grid()
 
-    final_train_mae = metrics_history['MAE'][-1]
-    test_mae = mean_absolute_error(model.decision_function(test_X), test_Y)
+print(f'The final train accuracy: {metrics_history["accuracy"][-1]}%')
+print(f'Test accuracy: {accuracy(model.decision_function(X_test), Y_test)}%')
 
-    print(f'The final train accuracy: {metrics_history["accuracy"][-1]}%')
-    print(f'Test accuracy: {accuracy(model.decision_function(test_X), test_Y)}%')
-    plt.show()
+plt.show()
