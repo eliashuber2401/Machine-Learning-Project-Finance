25b958b04b830c59f5ef48eaef4787fbb928699d
Minxuan Gu <michaelgu0105@gmail.com>
2025-07-07 13:57:54 +0200

    retest

--- Project main/import numpy as np.py
+++ Project main/import numpy as np.py
@@ -3,3 +3,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+
+
+print("This is a test file for git")
