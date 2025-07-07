77aaf0ca00efc4000db780c782c0369cf066874d
Minxuan Gu <michaelgu0105@gmail.com>
2025-07-07 13:58:42 +0200

    delete

--- Project main/import numpy as np.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd     
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
-
-
-print("This is a test file for git")
